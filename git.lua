f3ba2163876dd943a4f34307e9c21fd302a943a9
Chris Moore <christopher.e.moore@gmail.com>
2013-07-17 23:09:55 -0600

    wrapped everything in an xpcall, ensured lfs attributes existed

--- git.lua
+++ git.lua
@@ -3,20 +3,30 @@ local cmd = assert(..., "you forgot to specify a command")
 require 'lfs'
 
 local function recurse()
-	for f in lfs.dir('.') do
-		if f ~= '.' and f ~= '..' then
-			if lfs.attributes(f).mode == 'directory' then
-				if f == '.git' then
-					print(lfs.currentdir())
-					print('result',os.execute('git '..cmd))
+	local cwd = lfs.currentdir()
+	xpcall(function()
+		for f in lfs.dir('.') do
+			if f ~= '.' and f ~= '..' then
+				local attr = lfs.attributes(f)
+				if attr == nil then
+					io.stderr:write('failed to get attributes for '..cwd..'/'..f..'\n')
 				else
-					lfs.chdir(f)
-					recurse()
-					lfs.chdir('..')
+					if attr.mode == 'directory' then
+						if f == '.git' then
+							print(cwd)
+							print('result',os.execute('git '..cmd))
+						else
+							lfs.chdir(f)
+							recurse()
+							lfs.chdir('..')
+						end
+					end
 				end
 			end
 		end
-	end
+	end, function(err)
+		io.stderr:write(cwd..'\n'..err..'\n'..debug.traceback())
+	end)
 end
 
 recurse()
