c4d0e470f594528cccc6083a1d3c979f305672d7
Christopher Moore <christopher.e.moore@gmail.com>
2022-06-24 11:28:13 +0600

    fixed a windows-side bug

--- git.lua
+++ git.lua
@@ -2,14 +2,19 @@
 
 local cmd = assert(..., "you forgot to specify a command")
 
-require 'lfs'
+local lfs = require 'lfs'
+local getattr = lfs.symlinkattributes
+-- in Windows in lfs 1.8.0 there's an error inside of 'symlinkattributes'
+if not pcall(function() getattr'.' end) then
+	getattr = lfs.attributes
+end
 
 local function recurse()
 	local cwd = lfs.currentdir()
 	xpcall(function()
 		for f in lfs.dir('.') do
 			if f ~= '.' and f ~= '..' then
-				local attr = lfs.symlinkattributes(f)
+				local attr = getattr(f)
 				if attr == nil then
 					io.stderr:write('failed to get attributes for '..cwd..'/'..f..'\n')
 				else
