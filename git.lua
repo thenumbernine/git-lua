eca747c2c94a45437d1380c19be66aa6e861753e
Christopher Moore <christopher.e.moore@gmail.com>
2023-07-14 19:42:25 +0800

    changed it to run on rlistdir

--- git.lua
+++ git.lua
@@ -1,65 +1,44 @@
 #!/usr/bin/env lua
+local table = require 'ext.table'
+local file = require 'ext.file'
 
 local cmd = assert(..., "you forgot to specify a command")
 local io = require 'ext.io'	-- io.readproc
 
-local file = require 'ext.file'
-
 -- don't get stuck in recursion
 local checkedSoFar = {}
 
-local function recurse()
-	local cwd = file:cwd()
-	if checkedSoFar[cwd] then return end
-	checkedSoFar[cwd] = true
+local srcdir = file:cwd()
+local function handleGitDir(reqdir)
 	local err
-	-- TODO store here, launch later in a separate process, and somehow wait in Lua on multiple processes for any to finish
 	xpcall(function()
-		for f in file'.':dir() do
-			file(cwd):cd()
-			local attr = file(f):attr()
-			if attr == nil then
-				io.stderr:write('failed to get attributes for '..cwd..'/'..f..'\n')
+		file(reqdir):cd()
+		local cwd = file:cwd()
+		-- TODO how to resolve cwd wrt symlinks?
+		if checkedSoFar[cwd] then return end
+		checkedSoFar[cwd] = true
+
+		io.stderr:write(cwd, ' ... ')
+		local msg
+		msg, err = io.readproc('git '..cmd..' 2>&1')
+		if msg then
+			-- if it is a known / simple message
+			-- sometimes it's "Already up to date"
+			-- sometimes it's "Already up-to-date"
+			if msg:match'^Already up.to.date'
+			or msg:match'^There is no tracking information for the current branch'
+			then
+				--print first line only
+				print(msg:match'^([^\r\n]*)')
+			elseif msg:match"^On branch(.*)%s+Your branch is up.to.date with 'origin/(.*)'%.%s+nothing to commit, working tree clean" then
+				-- only for this one, go ahead and merge the first \n
+				print((msg:gsub('[\r\n]', ' ')))
 			else
-				if attr.mode == 'directory'
-				and not attr.target	-- and it's not a symlink
-				then
-					if f == '.git' then
-						io.stderr:write(cwd, ' ... ')
-						local msg, err = io.readproc('git '..cmd..' 2>&1')
-						if msg then
-							-- if it is a known / simple message
-							-- sometimes it's "Already up to date"
-							-- sometimes it's "Already up-to-date"
-							if msg:match'^Already up.to.date'
-							or msg:match'^There is no tracking information for the current branch'
-							then
-								--print first line only
-								print(msg:match'^([^\r\n]*)')
-							elseif msg:match"^On branch (.*)%s+Your branch is up to date with 'origin/(.*)'%.%s+nothing to commit, working tree clean" then
-								-- only for this one, go ahead and merge the first \n
-								print((msg:gsub('[\r\n]', ' ')))
-							else
-								-- print all output for things like pulls and conflicts and merges
-								print(msg)
-							end
-						else
-							error('ERROR '..err)
-						end
-					else
-						file(f):cd()
-						local err
-						xpcall(function()
-							recurse()
-						end, function(msg)
-							err = msg..'\n'..debug.traceback()
-						end)
-						file'..':cd()
-						-- TODO what about growing call stacks with directory depth?
-						if err then error(err) end
-					end
-				end
+				-- print all output for things like pulls and conflicts and merges
+				print(msg)
 			end
+		else
+			error('ERROR '..err)
 		end
 	end, function(msg)
 		err = msg..'\n'..debug.traceback()
@@ -67,6 +46,14 @@ local function recurse()
 	if err then
 		io.stderr:write(cwd..'\n'..err)
 	end
+	file(srcdir):cd()
 end
 
-recurse()
+file'.':rdir(function(f, isdir)
+	local dir, name = file(f):getdir()
+	if name == '.git' and isdir then
+		handleGitDir(dir)
+		return true	-- don't continue
+	end
+	return true
+end)
