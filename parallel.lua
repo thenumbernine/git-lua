a4cf3ee6f9d011796404954470f2ce16c7e6b1eb
Christopher Moore <christopher.e.moore@gmail.com>
2025-11-15 14:23:12 +0700

    got parallel impl working

--- parallel.lua
+++ parallel.lua
@@ -1,6 +1,7 @@
 #!/usr/bin/env luajit
 local ffi = require 'ffi'
 local table = require 'ext.table'
+local string = require 'ext.string'
 local path = require 'ext.path'
 local os = require 'ext.os'
 
@@ -17,7 +18,6 @@ local cmd = assert(..., "you forgot to specify a command")
 
 -- don't get stuck in recursion
 local checkedCWDs = {}
-local doneLookingForFiles
 local doneProcessingFiles
 local maxConcurrent = 4	-- = require 'thread'.numThreads()
 local tocheck = table()	-- mitigate these, only allow 4 checking at a time
@@ -32,18 +32,26 @@ xpcall(function()
 
 		local r = {}
 		r.dir = cwd
+		r.shell = os.tmpname()
 		r.output = os.tmpname()
 		r.done = os.tmpname()
-print('handleGitDir checking', r)
+		path(r.done):remove()	-- don't let it exist until we want it to exist
+
 		checking:insert(r)
-		print(require 'ext.tolua'(r))
 
 		if ffi.os == 'Linux' then
+			path(r.shell):write(table{
+				'luajit "'..luagitdir..'/parallel_url.lua" "'..cmd..'" > "'..r.output..'"',
+				'echo "done" >> "'..r.done..'"',
+			}:concat'\n'..'\n')
+
 			-- horrible parallelism ...
 			-- I should really find a package that does ipc / parallelism for me
 			-- hmmmmm why isn't the > working.
-			local cmd = '(luajit "'..luagitdir..'/parallel_url.lua" "'..cmd..'" &> "'..r.output..'"; echo "done" >> "'..r.done..'")  &'
-			os.exec(cmd)
+			local status, msg, errcode = os.execute('bash "'..r.shell..'" >/dev/null 2>/dev/null &')
+			if not status then
+				print(r.dir, 'failed with error', msg, errcode)
+			end
 		else
 			error("can't handle this OS yet ... running in sequence")
 		end
@@ -55,16 +63,11 @@ print('handleGitDir checking', r)
 	threads:add(function()
 		coroutine.yield()
 
-		while
---		not doneLookingForFiles or
-		--#checking > 0
-		true
-		do
+		while #checking > 0 or #tocheck > 0 do
 			-- see if we can add
 			if #tocheck > 0 and #checking < maxConcurrent then
 				local checkdir = tocheck:remove()
 				-- this'll add to 'checking'
-print('checking '..checkdir)
 				handleGitDir(checkdir)
 			end
 
@@ -73,13 +76,10 @@ print('checking '..checkdir)
 				for i=#checking,1,-1 do
 					local r = checking[i]
 					if path(r.done):exists() then	-- done
--- ...... but it's not done.
--- why is it writing 'done' before the output file even exists?
-						print('got results for '..r.dir)
-						print('out:', path(r.output):read())
-						print('removing...')
-						path(r.output):remove()
+						print(r.dir..' ... '..string.trim(path(r.output):read()))
 						path(r.done):remove()
+						path(r.output):remove()
+						path(r.shell):remove()
 						checking:remove(i)
 					end
 				end
@@ -91,23 +91,16 @@ print('checking '..checkdir)
 
 	for f in path'.':rdir(function(f, isdir)
 		local dir, name = path(f):getdir()
---print('searching', dir, name)
 		if name.path == '.git' and isdir then
---print('adding '..dir..' to list to check')
 			tocheck:insert(dir:fixpathsep())
 			return false	-- don't continue
 		end
 		return true
 	end) do end
 
-	threads:update()
-
-	print('done looking for files, now waiting for them to finish...')
-	doneLookingForFiles = true
-
-	threads:update()
-
-	print('done waiting for them to finish...')
+	while #threads.threads > 0 do
+		threads:update()
+	end
 
 end, function(err)
 	io.stderr:write(err,'\n',debug.traceback(),'\n')
