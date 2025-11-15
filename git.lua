c3a6d925d06c9402756c7833e52587d51ce0c5b5
Christopher Moore <christopher.e.moore@gmail.com>
2025-11-15 12:56:05 +0700

    making it modular?  not without some hiccups...

--- git.lua
+++ git.lua
@@ -1,66 +1,30 @@
 #!/usr/bin/env luajit
-local table = require 'ext.table'
 local path = require 'ext.path'
+local rundir = require 'git.rundir'
 
 local cmd = assert(..., "you forgot to specify a command")
-local io = require 'ext.io'	-- io.readproc
 
--- don't get stuck in recursion
-local checkedSoFar = {}
-
-local srcdir = path:cwd()
-local function handleGitDir(reqdir)
-	local err
-	xpcall(function()
-		path(reqdir):cd()
-		local cwd = path:cwd().path
-		-- TODO how to resolve cwd wrt symlinks?
-		if checkedSoFar[cwd] then return end
-		checkedSoFar[cwd] = true
-
-		-- TODO here launch a process and somehow wait for it to finish ...
-		-- how to monitor other than busy wait or temp files?
+-- TODO here launch a process and somehow wait for it to finish ...
+-- how to monitor other than busy wait or temp files?
 -- start /b git cmd 2^>^&1 ... but that still waits to exit right?
 
-		io.stderr:write(cwd, ' ... ')
-		local msg
-		msg, err = io.readproc('git '..cmd..' 2>&1')
-		if msg then
-			-- if it is a known / simple message
-			-- sometimes it's "Already up to date"
-			-- sometimes it's "Already up-to-date"
-			if msg:match'^Already up.to.date'
-			or msg:match'^Everything up.to.date'
-			or msg:match'^There is no tracking information for the current branch'
-			then
-				--print first line only
-				print(msg:match'^([^\r\n]*)')
-			elseif msg:match"^On branch(.*)%s+Your branch is up.to.date with 'origin/(.*)'%.%s+nothing to commit, working tree clean" then
-				-- only for this one, go ahead and merge the first \n
-				print((msg:gsub('[\r\n]', ' ')))
-			else
-				-- print all output for things like pulls and conflicts and merges
-				print(msg)
-			end
-		else
-			error('ERROR '..err)
-		end
-	end, function(msg)
-		err = msg..'\n'..debug.traceback()
-	end)
-	if err then
-		io.stderr:write(reqdir..'\n'..err)
-	end
-	srcdir:cd()
-end
-
+local srcdir = path:cwd()
+local checkedSoFar = {}
 for f in path'.':rdir(function(f, isdir)
 	local dir, name = path(f):getdir()
 	if name.path == '.git'
 	--and isdir	-- submodule .git folders are coming back in `stat` as regular files ...
 	then
-		handleGitDir(dir:fixpathsep())
-		return true	-- don't continue
+		xpcall(function()
+			rundir(cmd, dir:fixpathsep(), checkedSoFar)
+		end, function(err)
+			io.stderr:write(dir..'\n'..err..'\n'..debug.traceback())
+		end)
+		srcdir:cd()
+
+		--return false -- don't continue
+		return true		-- but what if there are subdirs
+		-- and if there are subdirs then doing child-first will cause parents' pull to always reset the children ...
 	end
 	return true
 end) do end
---
+++ rundir.lua
@@ -0,0 +1,40 @@
+local path = require 'ext.path'
+
+local io = require 'ext.io'	-- io.readproc
+
+-- does chdir
+-- uses it to resolve symlinks and check for duplicates / loops
+local function rundir(cmd, reqdir, checkedSoFar)
+	-- use cd+cwd to resolve symlinks
+	path(reqdir):cd()
+	local cwd = path:cwd().path
+
+	-- TODO how to resolve cwd wrt symlinks?
+	if checkedSoFar[cwd] then return end
+	checkedSoFar[cwd] = true
+
+	io.stderr:write(cwd, ' ... ')
+	local msg, err = io.readproc('git '..cmd..' 2>&1')
+	if msg then
+		-- if it is a known / simple message
+		-- sometimes it's "Already up to date"
+		-- sometimes it's "Already up-to-date"
+		if msg:match'^Already up.to.date'
+		or msg:match'^Everything up.to.date'
+		or msg:match'^There is no tracking information for the current branch'
+		then
+			--print first line only
+			print(msg:match'^([^\r\n]*)')
+		elseif msg:match"^On branch(.*)%s+Your branch is up.to.date with 'origin/(.*)'%.%s+nothing to commit, working tree clean" then
+			-- only for this one, go ahead and merge the first \n
+			print((msg:gsub('[\r\n]', ' ')))
+		else
+			-- print all output for things like pulls and conflicts and merges
+			print(msg)
+		end
+	else
+		error('ERROR '..err)
+	end
+end
+
+return rundir
